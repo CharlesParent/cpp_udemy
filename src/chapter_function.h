885524d65375c5a691650ef100e43cb3d479abfa
CharlesParent <charlespa@theodo.fr>
2018-04-20 08:35:29 +0200

    Add file for functions

---
+++ src/chapter_function.h
@@ -0,0 +1,12 @@
+#ifndef CHAPTER_FUNCTION
+#define CHAPTER_FUNCTION
+
+void breakAndContinue(void);
+void charTest(void);
+void loopTest(void);
+int infiniteLoop(void);
+void displayOnes(int, int);
+void declarationAndUsage(void);
+void enterAndReturn(void);
+
+#endif // !CHAPTER_FUNCTION
