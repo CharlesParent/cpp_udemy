497363acc261d09577e2ce389d9dd3c1087215b7
CharlesParent <charlespa@theodo.fr>
2018-04-27 22:45:58 +0200

    Arrays of structs

--- src/structures.cpp
+++ src/structures.cpp
@@ -23,16 +23,26 @@ static void displayDate(Date);
 
 void books(void)
 {
-    Book b1, b2;
-    b1.ID = 0;
-    b1.Name = "Fondation";
-    b1.Author = "Asimov";
-    b1.Count = 1;
+    Date dt_book1 = {27, 4, 2018}; 
+    Book b1 = {0, "Robot Cycle", "Asimov", dt_book1};
+    Book b2;
+    // b1.ID = 0;
+    // b1.Name = "Fondation";
+    // b1.Author = "Asimov";
+    // b1.Count = 1;
 
     std::cout << "B2 count value: " << b2.Count << std::endl; // Count shared by all books
     b2.Count = 10;
     std::cout << "B1 count value: " << b1.Count << std::endl; // Count shared by all books
     std::cout << "Books count value: " << Book::Count << std::endl; // Count shared by all books
+    std::cout << "Book1 author: " << b1.Author << std::endl;
+
+    // Arrays of structs
+    displayDate(b1.Published);
+    Book books[100];
+
+    books[2].ID = 10;
+    books[13].Author = "Hey";
 }
 
 void structures(void)
