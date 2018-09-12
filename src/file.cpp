5db0e726fafe039f3bc6058055f5d02100cd82fd
CharlesParent <charlespa@theodo.fr>
2018-09-12 14:43:23 +0200

    IOS Stream files

--- src/file.cpp
+++ src/file.cpp
@@ -3,10 +3,13 @@
 #include "file.h"
 
 void file(void){
-    std::cout << "Hello File" << std::endl;
-
-    std::ofstream f("test.txt");
-    f << "Hello World" << std::endl;
-
+    // Mode ios::app used to append content at the end of a file
+    std::ofstream f("test2.txt", std::ios::app);
+    // Check if we can access the file
+    if (!f)
+        std::cout << "File Does not exist "
+            "or you do not have the permission to write to it" << std::endl;
+    else
+        f << "Hello World" << std::endl;
     f.close();
 }
